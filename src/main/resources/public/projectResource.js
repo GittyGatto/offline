5c89531ce659b7130bc90a10a4a003a17fa6819f
GittyGatto <rhenning80@gmx.de>
2017-02-16 21:48:56 +0100

    header added. task crud under construction. some refactoring.

--- src/main/resources/public/projectResource.js
+++
@@ -1,9 +0,0 @@
-var offlineApp = angular.module('offlineApp');
-		
-offlineApp.factory('ProjectResource', [ '$resource', function($resource) {
-	return $resource('/project/:id', null, {
-		'update' : {
-			method : 'PUT'
-		}
-	});
-} ]);
--- src/main/resources/public/taskResource.js
+++
@@ -1,9 +0,0 @@
-var offlineApp = angular.module('offlineApp');
-		
-offlineApp.factory('TaskResource', [ '$resource', function($resource) {
-	return $resource('/task/:id', null, {
-		'update' : {
-			method : 'PUT'
-		}
-	});
-} ]);
