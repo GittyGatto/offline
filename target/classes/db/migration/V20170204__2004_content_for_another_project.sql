df3a13d75b5aab30b67dc7dd12ae82af6b115e9d
he <r.henning@arcusx.com>
2017-03-31 16:14:08 +0200

    Ignore target

--- target/classes/db/migration/V20170204__2004_content_for_another_project.sql
+++
@@ -1,4 +0,0 @@
-insert into project (name) values ('another project');
-insert into task (name, project_id) values ('junit tests', 2);
-insert into task (name, project_id) values ('bootstrap', 2);
-insert into task (name, project_id) values ('ui-route', 2);
